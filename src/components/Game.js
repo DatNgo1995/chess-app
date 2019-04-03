b93b79fee10bac07294626bbd49bc71dd8d16758
datngo1995 <datngotat1995@gmail.com>
2019-04-03 16:11:00 +0300

    no message

--- src/components/Game.js
+++ src/components/Game.js
@@ -24,10 +24,10 @@ export default class Game extends React.Component {
     if(this.state.sourceSelection === -1){
       if(!squares[i] || squares[i].player !== this.state.player){
         this.setState({status: "Wrong selection. Choose player " + this.state.player + " pieces."});
-        if (squares[i]) delete squares[i].style.backgroundColor
+        //if (squares[i]) delete squares[i].style.backgroundColor
       }
       else{
-        squares[i].style = {...squares[i].style, backgroundColor: "RGB(111,143,114)"}; // Emerald from http://omgchess.blogspot.com/2015/09/chess-board-color-schemes.html
+        //squares[i].style = {...squares[i].style, backgroundColor: "RGB(111,143,114)"}; // Emerald from http://omgchess.blogspot.com/2015/09/chess-board-color-schemes.html
         this.setState({
           status: "Choose destination for the selected piece",
           sourceSelection: i
@@ -36,7 +36,7 @@ export default class Game extends React.Component {
     }
 
     else if(this.state.sourceSelection > -1){
-      delete squares[this.state.sourceSelection].style.backgroundColor;
+      //delete squares[this.state.sourceSelection].style.backgroundColor;
       if(squares[i] && squares[i].player === this.state.player){
         this.setState({
           status: "Wrong selection. Choose valid source and destination again.",
@@ -132,10 +132,6 @@ export default class Game extends React.Component {
             </div>
             
           </div>
-        </div>
-
-        <div className="icons-attribution">
-          <div> <small> Chess Icons And Favicon (extracted) By en:User:Cburnett [<a href="http://www.gnu.org/copyleft/fdl.html">GFDL</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA-3.0</a>, <a href="http://opensource.org/licenses/bsd-license.php">BSD</a> or <a href="http://www.gnu.org/licenses/gpl.html">GPL</a>], <a href="https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces">via Wikimedia Commons</a> </small></div>
         </div>
       </div>
 
