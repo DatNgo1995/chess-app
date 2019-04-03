1570c9418d8221ff4b53a20efbcee2a6c355ff82
datngo1995 <datngotat1995@gmail.com>
2019-04-03 14:55:23 +0300

    no message

---
+++ src/components/FallenSoldierBlock.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../index.css';
+import Square from './Square';
+
+export default class FallenSoldierBlock extends React.Component {
+
+  renderSquare(square, i, squareShade) {
+    return <Square 
+    piece = {square} 
+    style = {square.style}
+    />
+  }
+
+  render() {
+    return (
+      <div>
+      <div className="board-row">{this.props.whiteFallenSoldiers.map((ws, index) =>
+        this.renderSquare(ws, index)
+        )}</div>
+      <div className="board-row">{this.props.blackFallenSoldiers.map((bs, index) =>
+        this.renderSquare(bs, index)
+        )}</div>
+      </div>
+      );
+  }
+}
