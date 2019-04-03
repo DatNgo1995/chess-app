f46611d65e286ec31cfe99997eafa022e5ff4b45
datngo1995 <datngotat1995@gmail.com>
2019-04-04 01:20:55 +0300

    implement castle

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import '../index.css';
-import Board from './Board';
-import FallenSoldierBlock from './FallenSoldierBlock';
-import initialiseChessBoard from './helper';
+import React from "react";
+import "../index.css";
+import Board from "./Board";
+import FallenSoldierBlock from "./FallenSoldierBlock";
+import initialiseChessBoard from "./helper";
+import Rook from './pieces/Rook';
 
 export default class Game extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       squares: initialiseChessBoard(),
@@ -13,105 +14,169 @@ export default class Game extends React.Component {
       blackFallenSoldiers: [],
       player: 1,
       sourceSelection: -1,
-      status: '',
-      turn: 'white',
-      enPassantPossible: -1 // for white : 24-31, for black 32-39
-    }
-  }
- 
-  handleClick(i){
-    
-    const squares = this.state.squares.slice();
-    if(this.state.sourceSelection === -1){
-      if(!squares[i] || squares[i].player !== this.state.player){
-        this.setState({status: "Wrong selection. Choose player " + this.state.player + " pieces."});
-        //if (squares[i]) delete squares[i].style.backgroundColor
+      status: "",
+      turn: "white",
+      enPassantPossible: -1, // for white : 24-31, for black 32-39
+      //check for castle
+      castlePosibility: {
+        rookA1Moved: false, //check if white left rook moved
+        rookA8Moved: false, //check if black right rook moved
+        rookH1Moved: false, //check if white left rook moved
+        rookH8Moved: false, //check if black right rook moved
+        kingE1Moved: false, //check if white king moved
+        kingE8Moved: false //check if black king moved
       }
-      else{
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (this.state.sourceSelection === -1) {
+      if (!squares[i] || squares[i].player !== this.state.player) {
+        this.setState({
+          status:
+            "Wrong selection. Choose player " + this.state.player + " pieces."
+        });
+        //if (squares[i]) delete squares[i].style.backgroundColor
+      } else {
         //squares[i].style = {...squares[i].style, backgroundColor: "RGB(111,143,114)"}; // Emerald from http://omgchess.blogspot.com/2015/09/chess-board-color-schemes.html
         this.setState({
           status: "Choose destination for the selected piece",
           sourceSelection: i
         });
       }
-    }
-
-    else if(this.state.sourceSelection > -1){
+    } else if (this.state.sourceSelection > -1) {
       //delete squares[this.state.sourceSelection].style.backgroundColor;
-      if(squares[i] && squares[i].player === this.state.player){
+      if (squares[i] && squares[i].player === this.state.player) {
         this.setState({
           status: "Wrong selection. Choose valid source and destination again.",
-          sourceSelection: -1,
+          sourceSelection: -1
         });
-      }
-      else{
-        
+      } else {
         const squares = this.state.squares.slice();
         const whiteFallenSoldiers = this.state.whiteFallenSoldiers.slice();
         const blackFallenSoldiers = this.state.blackFallenSoldiers.slice();
-        const isDestEnemyOccupied = squares[i]? true : false; 
-        const isMovePossible = squares[this.state.sourceSelection].isMovePossible(this.state.sourceSelection, i, 
-          isDestEnemyOccupied, this.state.enPassantPossible === i-8 || this.state.enPassantPossible === i+8);
-        const srcToDestPath = squares[this.state.sourceSelection].getSrcToDestPath(this.state.sourceSelection, i);
+        const isDestEnemyOccupied = squares[i] ? true : false;
+        const isMovePossible = squares[
+          this.state.sourceSelection
+        ].isMovePossible(
+          this.state.sourceSelection,
+          i,
+          isDestEnemyOccupied,
+          this.state.enPassantPossible === i - 8 ||
+            this.state.enPassantPossible === i + 8
+        );
+        const srcToDestPath = squares[
+          this.state.sourceSelection
+        ].getSrcToDestPath(this.state.sourceSelection, i);
         const isMoveLegal = this.isMoveLegal(srcToDestPath);
         let enPassantPossible = this.state.enPassantPossible;
-        console.log(srcToDestPath)
-        if(isMovePossible && isMoveLegal){
-          if(squares[i] !== null){
-            if(squares[i].player === 1){
+
+        if (isMovePossible && isMoveLegal) {
+          if (squares[i] !== null) {
+            if (squares[i].player === 1) {
               whiteFallenSoldiers.push(squares[i]);
-            }
-            else{
+            } else {
               blackFallenSoldiers.push(squares[i]);
             }
           }
-          else  if (((enPassantPossible >= 24 && enPassantPossible <=31 && i === enPassantPossible -8)
-          || (enPassantPossible >= 32 && enPassantPossible <=39 && i === enPassantPossible +8) )
-            && squares[this.state.sourceSelection].name==="pawn" ) {
-              squares[enPassantPossible] = null
+          // implement en passant
+          else if (
+            ((enPassantPossible >= 24 &&
+              enPassantPossible <= 31 &&
+              i === enPassantPossible - 8) ||
+              (enPassantPossible >= 32 &&
+                enPassantPossible <= 39 &&
+                i === enPassantPossible + 8)) &&
+            squares[this.state.sourceSelection].name === "pawn"
+          ) {
+            squares[enPassantPossible] = null;
           }
-          if (srcToDestPath.length===1 && squares[this.state.sourceSelection].name ==="pawn") {
-            this.setState({enPassantPossible: i})
+          else if ( squares[this.state.sourceSelection].name === "king" && srcToDestPath.length === 1 ){
+            if ( i > this.state.sourceSelection) {
+              squares[i+1] = null;
+              squares[i-1] = new Rook (this.state.player)
+              console.log(i, squares[i+3], squares[i+1])
+            }
+            else {
+              squares[i-2] = null;
+              squares[i+1] = new Rook (this.state.player)
+            }
           }
-          else {
-            this.setState({enPassantPossible: -1})
+          if (
+            srcToDestPath.length === 1 &&
+            squares[this.state.sourceSelection].name === "pawn"
+          ) {
+            this.setState({ enPassantPossible: i });
+          } else {
+            this.setState({ enPassantPossible: -1 });
           }
-          console.log("whiteFallenSoldiers", whiteFallenSoldiers) ;
+          console.log("whiteFallenSoldiers", whiteFallenSoldiers);
           console.log("blackFallenSoldiers", blackFallenSoldiers);
           squares[i] = squares[this.state.sourceSelection];
           squares[this.state.sourceSelection] = null;
-          let player = this.state.player === 1? 2: 1;
-          let turn = this.state.turn === 'white'? 'black' : 'white';
+          let player = this.state.player === 1 ? 2 : 1;
+          let turn = this.state.turn === "white" ? "black" : "white";
+          if (this.state.sourceSelection === 0) {
+            this.setState({
+              castlePosibility:{...this.state.castlePosibility,rookA8Moved: true}
+            });
+          }
+          if (this.state.sourceSelection === 7) {
+            this.setState({
+              castlePosibility:{...this.state.castlePosibility,rookH8Moved: true}
+            });
+          }
+          if (this.state.sourceSelection === 56) {
+            this.setState({
+              castlePosibility:{...this.state.castlePosibility,rookA1Moved: true}
+            });
+          }
+          if (this.state.sourceSelection === 63) {
+            this.setState({
+              castlePosibility:{...this.state.castlePosibility,rookH1Moved: true}
+            });
+          }
+          if (this.state.sourceSelection === 4) {
+            this.setState({
+              castlePosibility:{...this.state.castlePosibility,kingE8Moved: true}
+            });
+          }
+          if (this.state.sourceSelection === 60) {
+            this.setState({
+              castlePosibility:{...this.state.castlePosibility,kingE1Moved: true}
+            });
+          }
+
           this.setState({
             sourceSelection: -1,
             squares: squares,
             whiteFallenSoldiers: whiteFallenSoldiers,
             blackFallenSoldiers: blackFallenSoldiers,
             player: player,
-            status: '',
+            status: "",
             turn: turn
           });
-        }
-        else{
+        } else {
           this.setState({
-            status: "Wrong selection. Choose valid source and destination again.",
-            sourceSelection: -1,
+            status:
+              "Wrong selection. Choose valid source and destination again.",
+            sourceSelection: -1
           });
         }
       }
     }
-
   }
 
   /**
    * Check all path indices are null. For one steps move of pawn/others or jumping moves of knight array is empty, so  move is legal.
    * @param  {[type]}  srcToDestPath [array of board indices comprising path between src and dest ]
-   * @return {Boolean}               
+   * @return {Boolean}
    */
-  isMoveLegal(srcToDestPath){
+  isMoveLegal(srcToDestPath) {
     let isLegal = true;
-    for(let i = 0; i < srcToDestPath.length; i++){
-      if(this.state.squares[srcToDestPath[i]] !== null){
+    for (let i = 0; i < srcToDestPath.length; i++) {
+      if (this.state.squares[srcToDestPath[i]] !== null) {
         isLegal = false;
       }
     }
@@ -119,37 +184,34 @@ export default class Game extends React.Component {
   }
 
   render() {
-
     return (
       <div>
         <div className="game">
           <div className="game-board">
-            <Board 
-            squares = {this.state.squares}
-            onClick = {(i) => this.handleClick(i)}
+            <Board
+              squares={this.state.squares}
+              onClick={i => this.handleClick(i)}
             />
           </div>
           <div className="game-info">
             <h3>Turn</h3>
-            <div id="player-turn-box" style={{backgroundColor: this.state.turn}}>
-  
-            </div>
+            <div
+              id="player-turn-box"
+              style={{ backgroundColor: this.state.turn }}
+            />
             <div className="game-status">{this.state.status}</div>
 
             <div className="fallen-soldier-block">
-              
-              {<FallenSoldierBlock
-              whiteFallenSoldiers = {this.state.whiteFallenSoldiers}
-              blackFallenSoldiers = {this.state.blackFallenSoldiers}
-              />
-            }
+              {
+                <FallenSoldierBlock
+                  whiteFallenSoldiers={this.state.whiteFallenSoldiers}
+                  blackFallenSoldiers={this.state.blackFallenSoldiers}
+                />
+              }
             </div>
-            
           </div>
         </div>
       </div>
-
-     
-      );
+    );
   }
 }
--- src/components/pieces/King.js
+++ src/components/pieces/King.js
@@ -1,26 +1,53 @@
-import Piece from './Pieces';
+import Piece from "./Pieces";
 
 export default class King extends Piece {
-  constructor(player){
-    super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"));
+  constructor(player) {
+    super(
+      player,
+      player === 1
+        ? "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
+        : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"
+    );
   }
-  name ='king'
-  isMovePossible(src, dest){
-    return (src - 9 === dest || 
-      src - 8 === dest || 
-      src - 7 === dest || 
-      src + 1 === dest || 
-      src + 9 === dest || 
-      src + 8 === dest || 
-      src + 7 === dest || 
-      src - 1 === dest);
+  name = "king";
+
+  isMovePossible(src, dest, enPassant, castleProperty) {
+    return (
+      src - 9 === dest ||
+      src - 8 === dest ||
+      src - 7 === dest ||
+      src + 1 === dest ||
+      src + 9 === dest ||
+      src + 8 === dest ||
+      src + 7 === dest ||
+      src - 1 === dest ||
+      (src === 60 &&
+        dest === 62 &&
+        !castleProperty.kingE1Moved &&
+        !castleProperty.rookH1Moved) ||
+      (src === 60 &&
+        dest === 58 &&
+        !castleProperty.kingE1Moved &&
+        !castleProperty.rookA1Moved) ||
+      (src === 4 &&
+        dest === 6 &&
+        !castleProperty.kingE8Moved &&
+        !castleProperty.rookH8Moved) ||
+      (src === 4 &&
+        dest === 2 &&
+        !castleProperty.kingE1Moved &&
+        !castleProperty.rookA8Moved)
+    );
   }
 
   /**
    * always returns empty array because of one step
    * @return {[]}
    */
-  getSrcToDestPath(src, dest){
+  getSrcToDestPath(src, dest) {
+    if (src - dest === 2) return [src - 1];
+    if (src - dest === -2) return [src + 1];
+
     return [];
   }
 }
