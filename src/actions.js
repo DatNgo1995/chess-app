2894848fd78d0406427915a0bad9cdf848f95b28
datngo1995 <datngotat1995@gmail.com>
2019-04-09 02:26:57 +0300

    Update action for notCorrectSource producer

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,18 @@
-export const handleNotCorrectSource = (player) =>  
-    ({
-        type: "notCorrectSource",
-        payload: "Wrong selection. Choose player " + player + " pieces."
-    })
+export const handleNotCorrectSource = (isSourceSelectionNotCorrect, player, i) =>  
+    {
+        if (isSourceSelectionNotCorrect) {
+        return {
+            type: "notCorrectSource",
+            payload: "Wrong selection. Choose player " + player + " pieces."
+        }
+    }
+    else {
+        return {
+            type: "notCorrectSource",
+            payload:  "Choose destination for the selected piece"
+        }
+    }
+    }
+        
+    
+    
